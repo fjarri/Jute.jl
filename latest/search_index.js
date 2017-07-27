f8bd9270b3037309b2a43c7a0eebfc8a0a16a252
autodocs <autodocs>
2017-07-27 13:36:54 +0000

    build based on 4833b70

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Jute.runtests",
+    "page": "Home",
+    "title": "Jute.runtests",
+    "category": "Function",
+    "text": "Run the tests.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Jute,-a-Py.Test-inspired-testing-framework-1",
+    "page": "Home",
+    "title": "Jute, a Py.Test-inspired testing framework",
+    "category": "section",
+    "text": "Dummy docs, more to come.runtests"
+},
+
+]}
