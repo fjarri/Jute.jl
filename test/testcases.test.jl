e04172c0b19a7510946e2e4ef06b89dd7eb6249b
Bogdan Opanchuk <bogdan@opanchuk.net>
2017-08-10 23:48:37 +1000

    test: add some tests for the `testcases` part of the library

---
+++ test/testcases.test.jl
@@ -0,0 +1,65 @@
+module Testcases
+
+using Jute
+using DataStructures
+
+
+testcase_dependencies = testcase() do
+    fx1 = fixture() do produce
+        produce([1])
+    end
+
+    fx2 = fixture(fx1) do produce, x
+        produce([x])
+    end
+
+    fx3 = local_fixture(fx1) do produce, x
+        produce(x)
+    end
+
+    tc = testcase(fx2, fx3) do
+        @test 1 == 1
+    end
+
+    @test Jute.dependencies(tc) == OrderedSet([fx1, fx2])
+    @test Jute.parameters(tc) == [fx2, fx3]
+end
+
+
+simple_tagging = testcase() do
+    tc = testcase() do
+    end
+
+    @test Jute.tags(tc) == Set([])
+
+    tc =
+        tag(:tag1) <|
+        tag(:tag2) <|
+        tag(:tag1) <|
+        tc
+    @test Jute.tags(tc) == Set([:tag1, :tag2])
+
+    tc =
+        untag(:tag1) <|
+        tc
+    @test Jute.tags(tc) == Set([:tag2])
+end
+
+
+tag_untag_mixture = testcase() do
+    tc = testcase() do
+    end
+
+    # tagging commands are applied from inner to outer
+    tc =
+        tag(:tag1) <|
+        untag(:tag2) <|
+        untag(:tag1) <|
+        tag(:tag2) <|
+        tc
+
+    @test Jute.tags(tc) == Set([:tag1])
+end
+
+
+end
