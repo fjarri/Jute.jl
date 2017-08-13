baa307288ef09baf10f1e80be593b8b2c8383575
Bogdan Opanchuk <bogdan@opanchuk.net>
2017-08-13 19:31:15 +1000

    Add some reporting tests

--- test/TestUtils.jl
+++ test/TestUtils.jl
@@ -3,6 +3,8 @@ module TestUtils
 using Jute
 
 export nested_run
+export nested_run_with_output
+export match_text
 
 
 strip_colors(s) = replace(s, r"\e\[\d+m", "")
@@ -10,7 +12,11 @@ strip_colors(s) = replace(s, r"\e\[\d+m", "")
 
 function _nested_run(tcs, options, output_pass_through)
     run_options = build_run_options(options=options)
-    obj_dict = Dict(gensym("testcase") => tc for tc in tcs)
+    if isa(tcs, Array)
+        obj_dict = Dict(gensym("testcase") => tc for tc in tcs)
+    else
+        obj_dict = tcs
+    end
     exitcode, output = with_output_capture(output_pass_through) do
         Jute.runtests_internal(run_options, obj_dict)
     end
@@ -32,6 +38,70 @@ end
 
 function nested_run_with_output(tcs, options=nothing)
     _nested_run(tcs, options, false)
+end
+
+
+function escape_for_regex(s)
+    replace(s, r"([\.\[\]\(\)\\\*\+\?\^\$])", s"\\\1")
+end
+
+
+function normalize_template(s)
+    m = match(r"^(\s*)", s)
+    s = replace(s, Regex("^$(m[1])", "m"), "")
+    strip(s)
+end
+
+
+# FIXME: when the "fail description" functionality is available,
+# we need to use it here, so that the match fails are reported as fails and not errors.
+function match_text(template, text)
+    text_lines = split(strip(text), "\n")
+    template_lines = split(normalize_template(template), "\n")
+
+    text_i = 1
+    template_i = 1
+
+    r_multiline = r"^<<<MULTILINE>>>$"
+    r_variable = r"<<<([\w\d_]+)>>>"
+
+    skipping_multiline = false
+    while true
+        if text_i > length(text_lines) && template_i > length(template_lines)
+            break
+        elseif text_i > length(text_lines)
+            error("Exhausted text")
+        elseif template_i > length(template_lines)
+            if skipping_multiline
+                break
+            end
+            error("Exhausted template")
+        end
+
+        text_line = text_lines[text_i]
+        template_line = template_lines[template_i]
+
+        if ismatch(r_multiline, template_line)
+            skipping_multiline = true
+            template_i += 1
+        else
+            s = "^" * escape_for_regex(template_line) * "\$"
+            s = replace(s, r_variable, s"(?<\1>.*)")
+            if !ismatch(Regex(s), text_line)
+                if !skipping_multiline
+                    error("Failed to match line\n$text_line\nwith\n$s")
+                else
+                    text_i += 1
+                end
+            else
+                skipping_multiline = false
+                text_i += 1
+                template_i += 1
+            end
+        end
+    end
+
+    true
 end
 
 
---
+++ test/reporting.test.jl
@@ -0,0 +1,145 @@
+module Reporting
+
+using Jute
+using TestUtils
+
+
+TESTCASES = Dict(
+    :multiple_tests => testcase() do
+        @test 1 == 1
+        @test 2 == 2
+        @test 3 == 3
+    end,
+
+    :returning_value => testcase() do
+        @test 1 == 1
+        @test_result 10
+    end,
+
+    :multiple_tests_and_one_failure => testcase() do
+        @test 1 == 1
+        @test 2 == 1
+        @test 3 == 3
+    end,
+
+    :uncaught_exception => testcase() do
+        @test 1 == 1
+        error("Uncaught exception")
+        @test 1 == 1
+    end,
+
+    :caught_exception => testcase() do
+        @test 1 == 1
+        @test_throws ErrorException error("Caught exception")
+        @test 1 == 1
+    end,
+
+    :skip_test => testcase() do
+        @test 1 == 1
+        @test_skip 1 == 2
+        @test 1 == 1
+    end,
+
+    :expected_failure => testcase() do
+        @test 1 == 1
+        @test_broken 1 == 2
+        @test 1 == 1
+    end,
+
+    :unexpected_pass => testcase() do
+        @test 1 == 1
+        @test_broken 1 == 1
+        @test 1 == 1
+    end,
+    )
+
+
+verbosity0 = testcase() do
+    exitcode, output = nested_run_with_output(TESTCASES, Dict(:verbosity => 0))
+    @test exitcode == 1
+    @test strip(output) == ""
+end
+
+
+verbosity1 = testcase() do
+    exitcode, output = nested_run_with_output(TESTCASES, Dict(:verbosity => 1))
+    @test exitcode == 1
+
+    template = """
+        Collecting testcases...
+        Running 8 out of 8 testcases...
+        ================================================================================
+        Platform: Julia <<<julia_version>>>, Jute <<<jute_version>>>
+        --------------------------------------------------------------------------------
+        ....*.F..E....B..B..E.
+        --------------------------------------------------------------------------------
+        17 tests passed, 1 failed, 2 errored in <<<full_time>>> (total test time <<<test_time>>>)
+        ================================================================================
+        multiple_tests_and_one_failure
+        Test Failed
+          Expression: 2 == 1
+           Evaluated: 2 == 1
+        ================================================================================
+        uncaught_exception
+        Error During Test
+          Got an exception of type ErrorException outside of a @test
+          Uncaught exception
+          Stacktrace:
+        <<<MULTILINE>>>
+        ================================================================================
+        unexpected_pass
+        Error During Test
+         Unexpected Pass
+         Expression: 1 == 1
+         Got correct result, please change to @test if no longer broken.
+    """
+
+    @test match_text(template, output)
+end
+
+
+verbosity2 = testcase() do
+    exitcode, output = nested_run_with_output(TESTCASES, Dict(:verbosity => 2))
+    @test exitcode == 1
+
+    template = """
+        Collecting testcases...
+        Running 8 out of 8 testcases...
+        ================================================================================
+        Platform: Julia <<<julia_version>>>, Jute <<<jute_version>>>
+        --------------------------------------------------------------------------------
+        multiple_tests (<<<time>>>) [PASS] [PASS] [PASS]
+        returning_value (<<<time>>>) [PASS] [10]
+        multiple_tests_and_one_failure (<<<time>>>) [PASS] [FAIL] [PASS]
+        uncaught_exception (<<<time>>>) [PASS] [ERROR]
+        caught_exception (<<<time>>>) [PASS] [PASS] [PASS]
+        skip_test (<<<time>>>) [PASS] [BROKEN] [PASS]
+        expected_failure (<<<time>>>) [PASS] [BROKEN] [PASS]
+        unexpected_pass (<<<time>>>) [PASS] [ERROR] [PASS]
+        --------------------------------------------------------------------------------
+        17 tests passed, 1 failed, 2 errored in <<<full_time>>> (total test time <<<test_time>>>)
+        ================================================================================
+        multiple_tests_and_one_failure
+        Test Failed
+          Expression: 2 == 1
+           Evaluated: 2 == 1
+        ================================================================================
+        uncaught_exception
+        Error During Test
+          Got an exception of type ErrorException outside of a @test
+          Uncaught exception
+          Stacktrace:
+        <<<MULTILINE>>>
+        ================================================================================
+        unexpected_pass
+        Error During Test
+         Unexpected Pass
+         Expression: 1 == 1
+         Got correct result, please change to @test if no longer broken.
+    """
+
+    @test match_text(template, output)
+end
+
+
+end
