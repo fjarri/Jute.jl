a4aff83d7f04d2a89fd29e8a8b043f394c4ff0db
Bogdan Opanchuk <bogdan@opanchuk.net>
2017-08-30 00:06:30 +1000

    testcases: add an option description in the docstring of testcase()

--- src/testcases.jl
+++ src/testcases.jl
@@ -93,6 +93,10 @@ end
     @testcase [option=val ...] <name> for x in fx1, y in fx2 ... end
 
 Create a testcase object and add it to the current test group.
+
+Available options:
+
+`tags :: Array{Symbol, 1}`: a list of tags for the testcase.
 """
 macro testcase(args...)
 
