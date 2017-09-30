c6f5fb8451f985d533bbb4110d28c552de4c3f1d
Bogdan Opanchuk <bogdan@opanchuk.net>
2017-10-01 00:21:35 +1000

    test/runtest_benchmark_testcases: no need for `eval()`

--- test/performance/runtests_benchmark_testcases.jl
+++ test/performance/runtests_benchmark_testcases.jl
@@ -11,9 +11,7 @@ end
 f3 = [1, 2, 3]
 
 for i in 1:100
-    eval(quote
-        @testcase $("t$i") for x in f1, y in f2, z in f3
-            @test 1 == 1
-        end
-    end)
+    @testcase "t$i" for x in f1, y in f2, z in f3
+        @test 1 == 1
+    end
 end
