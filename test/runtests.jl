f915464883ce3622f99514710987d74d0b02942b
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-06-14 13:28:14 +1000

    Back to specifying tests explicitly
    TestUtils needs to be loaded first, which is the case on MacOs, but not on Linux, because of the different filename sorting.
    If one wants to use dynamic loading, they have to rename it to `_TestUtils.test.jl` or something.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,16 @@
 using Jute
 
+include("TestUtils.jl")
+
+include("builtin_fixtures.test.jl")
+include("fixture_factory.test.jl")
+include("fixtures.test.jl")
+include("options.test.jl")
+include("performance.test.jl")
+include("reporting.test.jl")
+include("run_testcase.test.jl")
+include("runtests.test.jl")
+include("testcases.test.jl")
+include("utils.test.jl")
+
 exit(runtests())
