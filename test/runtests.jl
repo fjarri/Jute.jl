90b27ccba2f9fd0a8b548b5e5b634cd69b64b136
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-06-14 13:18:02 +1000

    Return to dynamic loading of testcases
    Because of the changes in Pkg3, one cannot import packages not listed as a dependency.
    They can still be included explicitly though. So we are including the TestUtils file by marking it as a test file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,3 @@
 using Jute
 
-# FIXME: At the moment dynamic loading does not work with v0.7
-# It seems that one cannot load packages not listed as dependencies.
-# So, for the time being, we're including the test files explicitly.
-
-include("TestUtils.jl")
-
-include("builtin_fixtures.test.jl")
-include("fixture_factory.test.jl")
-include("fixtures.test.jl")
-include("options.test.jl")
-include("performance.test.jl")
-include("reporting.test.jl")
-include("run_testcase.test.jl")
-include("runtests.test.jl")
-include("testcases.test.jl")
-include("utils.test.jl")
-
 exit(runtests())
